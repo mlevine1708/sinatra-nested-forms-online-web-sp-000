3baccfc06cfb3abe74a5be4d61e3f74a2d56eec0
Micah Levine <micah.levine@outlook.com>
2020-01-20 15:24:21 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,5 +2,17 @@ class Pirate
 
   attr_accessor  :name, :weight, :height
 
-  def initialize 
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
